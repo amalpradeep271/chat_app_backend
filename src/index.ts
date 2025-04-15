ce730e34d854196d6028861c89afc8958d6618d8
Amal Pradeep <amalpradeep271@gmail.com>
2025-04-15 13:45:39 +0530

    added some backend functionalities regarding conversations

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { json } from "body-parser";
 import authRoutes from "./routes/authRoutes";
-import express, { Request, Response } from 'express';
+import conversationsRoutes from "./routes/conversationsRoutes";
 
+import express, { Request, Response } from "express";
 
 const app = express();
 app.use(json());
 
 app.use("/auth", authRoutes);
+app.use("/conversations", conversationsRoutes);
 
 const PORT = process.env.PORT || 6000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { error } from "console";
+import { Request, Response, NextFunction } from "express";
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    res.status(403).json({ error: "No token provided" });
+    return;
+  }
+  
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN || "worisecretkey");
+    req.user =decoded as {id: string};
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+};
