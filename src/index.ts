b098e2a14ef17d8b06751636baf27c26438c481b
Amal Pradeep <amalpradeep271@gmail.com>
2025-04-16 12:28:24 +0530

    implemented the socket io connection for backend and for the real time chat features from npm

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,49 @@ import { json } from "body-parser";
 import authRoutes from "./routes/authRoutes";
 import conversationsRoutes from "./routes/conversationsRoutes";
 import messagesRoutes from "./routes/messagesRoutes";
-
+import http from "http";
+import { Server } from "socket.io";
 import express, { Request, Response } from "express";
+import { saveMessage } from "./controllers/messageControllers";
+import { error } from "console";
 
 const app = express();
+const server = http.createServer(app);
 app.use(json());
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use("/auth", authRoutes);
 app.use("/conversations", conversationsRoutes);
 app.use("/messages", messagesRoutes);
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+  socket.on("joinConversation", (conversationId) => {
+    socket.join(conversationId);
+    console.log("User joined conversation :" + conversationId);
+  });
+
+  socket.on("SendMessage", async (message) => {
+    const { conversationId, senderId, content } = message;
+
+    try {
+      const savedMessage = await saveMessage(conversationId, senderId, content);
+      console.log("sendMessage : ");
+      console.log(savedMessage);
+      io.to(conversationId).emit("newMessage", saveMessage);
+    } catch (err) {
+      console.error("Failed to save message", error);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
