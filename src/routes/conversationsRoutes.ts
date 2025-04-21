3dc6c3cd21166683173dd345e72ab724e8727b66
Amal Pradeep <amalpradeep271@gmail.com>
2025-04-21 23:19:43 +0530

    check conversation code added

--- src/routes/conversationsRoutes.ts
+++ src/routes/conversationsRoutes.ts
@@ -1,8 +1,10 @@
-import { fetchAllConversationsByUserId } from "../controllers/conversationsController";
+import { checkOrCreateConversation, fetchAllConversationsByUserId } from "../controllers/conversationsController";
 import { verifyToken } from "../middlewares/authMiddleware";
 import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", verifyToken, fetchAllConversationsByUserId);
+router.post("/check-or-create", verifyToken, checkOrCreateConversation);
+
 
 export default router;
