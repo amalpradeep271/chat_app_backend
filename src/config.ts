bf07765911317508801c30ff91c48c3d45545ae9
Amal Pradeep <amalpradeep271@gmail.com>
2025-04-25 21:44:15 +0530

    uncommented config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-// export const openaiKey = "";
-// export const AI_BOT_ID = "00000000-0000-0000-0000-000000000001";
+export const openaiKey = "";
+export const AI_BOT_ID = "00000000-0000-0000-0000-000000000001";
